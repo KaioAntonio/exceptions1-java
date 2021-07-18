4a077e3421530b388ab43e397499e94d20b4c665
KaioAntonio <kaioaar@gmail.com>
2021-07-18 16:55:11 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -32,31 +31,22 @@ public class Program {
 			
 			System.out.println();
 			System.out.println("Enter data to update the reservation: ");
-			System.out.println("Room number: ");
-			number = sc.nextInt();
 			System.out.println("Check-in date: (dd/MM/yyyy): ");
 			checkIn =  sdf.parse(sc.next());
 			System.out.println("Check-out date: (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
+			String error = reservation.updateDates(checkIn, checkOut);
 			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 				
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-				
-			}
+			}	
 			else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: "+reservation);
+				System.out.println("Reservation: " + reservation);
 			}
-			
 			
 		}
-		
 		sc.close();
 
 	}
